a4345f0d393dc247d26fd228ac48d4706c7264f8
alriffaud <alriffaud@gmail.com>
2024-01-24 13:28:35 +0000

    Modification of 8-uppercase.p file.

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def uppercase(c):
-    if ord(c) >= 65 and ord(c) <= 90:
-        return (True)
-    else:
-        return (False)
+def uppercase(str):
+    for i in range(len(str)):
+        n = ord(str[i])
+        print("{}".format(chr(n - 32) if n >= 97 and n <= 122 else str[i]),
+              end="")
+    print("")
