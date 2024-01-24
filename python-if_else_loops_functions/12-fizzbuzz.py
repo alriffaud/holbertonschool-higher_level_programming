26c0d9238ed3b60baccbef382f006a03c96d5b17
Alberto Riffaud <alriffaud@gmail.com>
2024-01-24 19:24:32 -0300

    Modification of 12-fizzbuzz.py file.

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""
-This function prints the numbers from 1 to 100 separated by a space.
-For multiples of three print Fizz instead of the number and for multiples
-of five print Buzz.
-For numbers which are multiples of both three and five print FizzBuzz.
-"""
 def fizzbuzz():
+    """
+    This function prints the numbers from 1 to 100 separated by a space.
+    For multiples of three print Fizz instead of the number and for multiples
+    of five print Buzz.
+    For numbers which are multiples of both three and five print FizzBuzz.
+    """
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
@@ -15,4 +15,3 @@ def fizzbuzz():
             print("Buzz", end=" ")
         else:
             print("{}".format(i), end=" ")
-    print("$", end="")
