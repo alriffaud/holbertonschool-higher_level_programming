28f4a088ed6399e95ddc9cd8b7c27900622cc809
alriffaud <100241090+alriffaud@users.noreply.github.com>
2024-01-26 20:36:38 -0300

    Update 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -5,7 +5,7 @@ def multiple_returns(sentence):
     character.
     """
     if sentence == "":
-        tuple_sentence = (0,"None")
+        tuple_sentence = (0, "None")
     else:
         tuple_sentence = (len(sentence), sentence[0])
     return (tuple_sentence)
