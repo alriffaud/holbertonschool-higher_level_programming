3d5d1a0e958e8b516155f31c6bb6db4b24c6db5d
Alberto Riffaud <alriffaud@gmail.com>
2024-01-26 20:31:40 -0300

    Creation of 8-multiple_returns.py file

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """
+    This function returns a tuple with the length of a string and its first
+    character.
+    """
+    tuple_sentence = (len(sentence), sentence[0])
+    return (tuple_sentence)
