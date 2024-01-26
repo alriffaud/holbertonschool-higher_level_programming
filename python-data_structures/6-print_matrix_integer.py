f31fc6d3f0ab86999db75cc2a80e2c6d96759433
Alberto Riffaud <alriffaud@gmail.com>
2024-01-26 18:05:42 -0300

    Creation of 6-print_matrix_integer.py file

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,13 @@ def print_matrix_integer(matrix=[[]]):
     """
     This function prints a matrix of integers.
     """
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            print("{:d}".format(matrix[i][j]), end=" ")
-        print()
+    m = len(matrix)
+    #n = len(matrix[0])
+    for row in matrix:
+        n = len(row)
+        for j in range(n - 1):
+            print("{:d}".format(row[j]), end=" ")
+        if n > 0:
+            print("{:d}".format(row[n - 1]))
+        else:
+            print()
