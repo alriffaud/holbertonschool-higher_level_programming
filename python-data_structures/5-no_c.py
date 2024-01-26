9b4020377f5aeb9cdf37640535c1e402a24ec099
Alberto Riffaud <alriffaud@gmail.com>
2024-01-26 17:19:17 -0300

    Creation of 5-no_c.py file

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """
+    This function removes all characters c and C from a string.
+    """
+    new_str = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_str += i
+    return (new_str)
