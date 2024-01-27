8c04156c50e6142809ed0ccb4c0d3ef5ae0e8d90
Alberto Riffaud <alriffaud@gmail.com>
2024-01-27 14:16:16 -0300

    Modification of 102-magic_calculation.py file.

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+def default_sub(a, b):
+    return (a - b)
+
+
 def magic_calculation(a, b):
-    sub = None
+    sub = default_sub  # Default subtraction function
     if a < b:
-        from magic_calculation_102 import add, sub
+        from magic_calculation_102 import add, sub as sub_function
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
