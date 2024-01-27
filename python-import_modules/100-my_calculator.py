020e4149594f9bf761023effa3efe48dc4bca18d
Alberto Riffaud <alriffaud@gmail.com>
2024-01-27 13:27:25 -0300

    Creation of 100-my_calculator.py file.

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
+    operators = ["+", "-", "*", "/"]
+    if operator not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    if operator == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif operator == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif operator == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
