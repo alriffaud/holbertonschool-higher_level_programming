1b2852f930877caab52fda1fd1b3bfb18f03e972
Alberto Riffaud <alriffaud@gmail.com>
2024-01-30 10:04:34 -0300

    Creation of README.md and 0-square_matrix_simple.py files.

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def copy_matrix(matrix=[[]]):
+    """
+    This function copies a matrix of integers.
+    """
+    new_matrix = []
+    m = len(matrix)
+    for row in range(m):
+        new_row = []
+        n = len(matrix[row])
+        for j in range(n):
+            new_row.append(matrix[row][j])
+        new_matrix.append(new_row)
+    return (new_matrix)
+
+def square_matrix_simple(matrix=[]):
+    """
+    This function computes the square value of all integers of a matrix.
+    """
+    new_matrix = copy_matrix(matrix)
+    m = len(new_matrix)
+    for row in range(m):
+        n = len(new_matrix[row])
+        for j in range(n):
+            new_matrix[row][j] = new_matrix[row][j] ** 2
+    return (new_matrix)
