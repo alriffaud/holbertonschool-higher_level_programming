cb2f5706b780861fbc01b3475fada58cc13f10fd
Alberto Riffaud <alriffaud@gmail.com>
2024-02-01 16:47:07 -0300

    Creation of 1-square.py file.

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+In this module, a Square class is defined.
+"""
+
+
+class Square:
+    """
+    This class defines a square.
+    Attributes:
+    - __size: This is the size of the square.
+    """
+    def __init__(self, size):
+        self.__size = size
