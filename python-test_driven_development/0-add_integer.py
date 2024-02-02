04ac2756253e7f6fe5926150ff04dabf22762832
Alberto Riffaud <alriffaud@gmail.com>
2024-02-02 14:36:07 -0300

    Modification of 0-add_integer.py and tests/0-add_integer.txt files.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,8 +11,8 @@ def add_integer(a, b=98):
     Returns:
     - The sum of a and b.
     """
-    if not isinstance(a, int) and not isinstance(a, float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
