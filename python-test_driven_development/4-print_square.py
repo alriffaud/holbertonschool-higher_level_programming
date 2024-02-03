31e33352d4e0c7ecdb7c756d4e9e57d4e52611b2
Alberto Riffaud <alriffaud@gmail.com>
2024-02-03 15:18:23 -0300

    Creation of 4-print_square.py and tests/4-print_square.txt files.

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module defines the print_square function.
+Functions:
+- print_square: This function prints a square with the character #.
+"""
+
+
+def print_square(size):
+    """
+    This function prints a square with the character #.
+    Parameters:
+    - size (int): Represents the size length of the square.
+    Returns:
+    - None.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size == 0:
+        print()
+    for row in range(size):
+        print("#" * size)
