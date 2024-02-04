d0c6e51186192ee22222554484c27bd56d94b32e
Alberto Riffaud <alriffaud@gmail.com>
2024-02-04 14:37:25 -0300

    Modification of 1-search_replace.py file.

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,7 +4,5 @@ def search_replace(my_list, search, replace):
     This function replaces all occurrences of an element by another in a new
     list.
     """
-    if not my_list:
-        return (None)
     new_list = [replace if x == search else x for x in my_list]
     return (new_list)
