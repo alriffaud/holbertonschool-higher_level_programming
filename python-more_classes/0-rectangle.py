49294c7955f0b738649002e51ee78865cce6de44
Alberto Riffaud <alriffaud@gmail.com>
2024-02-05 10:50:44 -0300

    Creation of EADME.md and 0-rectangle.py files.

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+In this module, an empty class is defined.
+Classes:
+- Rectangle: This class defines a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    This is an empty class that defines a rectangle.
+    """
+    pass
