0e2b277f761e14e4148ca1906b6fc3879f37bb50
alriffaud <alriffaud@gmail.com>
2024-02-07 14:16:16 +0000

    Creation of 5-base_geometry.py file.

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+In this module, an empty class is defined.
+"""
+
+
+class BaseGeometry:
+    """
+    This is an empty class that defines a BaseGeometry.
+    """
+    pass
