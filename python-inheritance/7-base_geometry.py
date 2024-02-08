2fe25fb1ca4f292ca288f1f3fe7a62bb4656b563
Alberto Riffaud <alriffaud@gmail.com>
2024-02-08 11:22:25 -0300

    Modification of 7-base_geometry.py and tests/7-base_geometry.txt files.

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
+"""
+module for BaseGeometry.
+"""
 
 
 class BaseGeometry():
-    """for use with shapes. Super class.
-    """
-
+    """BaseGeometry class"""
+    
     def area(self):
-        """instance method to calculate area of shape
-        """
+        """Raise an Exception"""
         raise Exception("area() is not implemented")
-
+    
     def integer_validator(self, name, value):
-        """validates integer input
-        """
+        """Validate"""
         if type(value) != int:
             raise TypeError(name + " must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError(name + " must be greater than 0")
