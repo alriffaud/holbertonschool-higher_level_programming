2866305d76f347801cb04d8ce87ff81a173a4293
Alberto Riffaud <alriffaud@gmail.com>
2024-02-08 09:16:31 -0300

    Creation of 7-base_geometry.py and tests/7-base_geometry.txt files.

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+In this module, the class BaseGeometry is defined.
+"""
+
+
+class BaseGeometry:
+    """
+    This is the class that defines a BaseGeometry.
+    Methods:
+    - area(self):
+    """
+    def area(self):
+        """
+        This method raises an Exception with the message area() is not
+        implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        This method validates the variable value.
+        Parameters:
+        - name(str): It's the of the BaseGeometry.
+        - value(int): It's  the value of the BaseGeometry
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
