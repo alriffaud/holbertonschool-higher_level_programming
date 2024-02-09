daf97bb6ad6af8ec87bd011db90f99f51c6d03d3
Alberto Riffaud <alriffaud@gmail.com>
2024-02-08 21:05:17 -0300

    Creation of 10-square.py file.

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+In this module, the class Rectangle is defined.
+Classes:
+- Rectangle: This defines the Rectangle class.
+- Square: This defines the Square class.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    This class inherits since BaseGeometry and defines a rectangle.
+    Attributes:
+    - __width: This is the width of the rectangle.
+    - __height: This is the height of the rectangle.
+    """
+    def __init__(self, width, height):
+        """
+        This is the initialization method.
+        Parameters:
+          - width: This is the width of the rectangle.
+          - height: This is the height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        This method returns the area of the rectangle.
+        """
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """
+        This method prints and returns a rectangle description.
+        Returns:
+        - A string with the rectangle.
+        """
+        return ("[" + Rectangle.__name__ + "] " + str(self.__width)
+                + "/" + str(self.__height))
+
+
+class Square(Rectangle):
+    """
+    This class inherits since BaseGeometry and defines a square.
+    Attributes:
+    - __size: This is the size of the square.
+    """
+    def __init__(self, size):
+        """
+        This method returns a new Rectangle instance with width == height ==
+        size.
+        Parameters:
+        - size: It's the size of the rectangle instance.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        This method returns the area of the square.
+        """
+        return (self.__size ** 2)
