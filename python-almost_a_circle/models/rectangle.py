c5d464d0f3e696a5fcf62064f29ada9b4103edbc
Alberto Riffaud <alriffaud@gmail.com>
2024-02-12 14:55:23 -0300

    Modification of models/rectangle.py file.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,14 +28,6 @@ class Rectangle(Base):
           - x: This is the x coordinate.
           - y: This is the y coordinate.
         """
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
         self.__x = x
@@ -56,10 +48,6 @@ class Rectangle(Base):
         Parameters:
           - value: It's the new width value.
         """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -76,8 +64,4 @@ class Rectangle(Base):
         Parameters:
           - value: It's the new height value.
         """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
         self.__height = value
