c0fd1143764240bd8963454952d7752795678839
Alberto Riffaud <alriffaud@gmail.com>
2024-02-15 10:52:19 -0300

    Modification of models/square.py file.

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -76,6 +76,8 @@ class Square(Rectangle):
         """
         This method returns the dictionary representation of a Square.
         """
+        if self is None:
+            return (None)
         return {
             'id': self.id,
             'size': self.width,
