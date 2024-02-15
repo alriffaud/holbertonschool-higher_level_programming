3067a6d8d9cdd775793831f8259f8288d34b50d5
Alberto Riffaud <alriffaud@gmail.com>
2024-02-15 14:03:30 -0300

    Modification of models/base.py file.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -56,3 +56,14 @@ class Base:
         json_list = [obj.to_dictionary() for obj in list_objs]
         with open(filename, 'w', encoding="utf-8") as file:
             file.write(cls.to_json_string(json_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        This method returns the list of the JSON string representation.
+        Parameters:
+        - json_string:  is a string representing a list of dictionaries.
+        """
+        if json_string is None or json_string == "[]":
+            return ([])
+        return (json.loads(json_string))
