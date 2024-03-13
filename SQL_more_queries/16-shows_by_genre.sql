8fb1e44795aa5b58ae67a27e65164e5386c759f1
alriffaud <alriffaud@gmail.com>
2024-03-13 18:19:14 +0000

    Creation of 16-shows_by_genre.sql file.

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server.
-SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS genre_name
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+ORDER BY title, name ASC;
