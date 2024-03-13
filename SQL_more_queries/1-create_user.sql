9293308fe1bc7b3feaf2b881be5d0ad34ad69509
alriffaud <alriffaud@gmail.com>
2024-03-13 14:32:46 +0000

    Modification of 1-create_user.sql file.

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,6 +2,6 @@
 -- user_0d_1 should have all privileges on your MySQL server
 -- The user_0d_1 password should be set to user_0d_1_pwd
 -- If the user user_0d_1 already exists, your script should not fail
-CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+CREATE USER '$username'@'localhost' IDENTIFIED BY '$password';
+GRANT ALL PRIVILEGES ON *.* TO '$username'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
