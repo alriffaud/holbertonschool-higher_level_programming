f86bfd3c743e7596210b5e078a9a5ceb838f808a
Alberto <alriffaud@gmail.com>
2024-03-15 13:41:37 -0300

    Modification of 2-my_filter_states.py file.

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -6,28 +6,21 @@ table of hbtn_0e_0_usa where name matches the argument.
 import MySQLdb
 from sys import argv
 
-
-def main(argv):
-    """
-        Function main
-    """
-    if len(argv) - 1 != 4:
-        print("Must enter 4 arguments")
-        return
-
-    db = MySQLdb.connect(host="localhost",
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
-                         port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
-    for row in cur.fetchall():
-        if row[1] == argv[4]:
-            print(row)
-    db.close()
-
-
 if __name__ == "__main__":
-    import sys
-    main(sys.argv)
+    db = MySQLdb.connect(
+                            host="localhost",
+                            port=3306,
+                            user=argv[1],
+                            password=argv[2],
+                            db=argv[3],
+                            )
+    cur = db.cursor()
+    cur.execute("SELECT *\
+                FROM states\
+                WHERE name\
+                LIKE BINARY '{}'\
+                ORDER BY id ASC".format(argv[4]))
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
