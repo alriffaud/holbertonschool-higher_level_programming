e3a6ff6ffeec2882b2c88b8dccc3915d19ef08b3
Alberto <alriffaud@gmail.com>
2024-03-15 19:50:10 -0300

    Modification of 4-cities_by_state.py file.

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
                             db=argv[3],
                             )
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id")
     for row in cur.fetchall():
         print(row)
     cur.close()
