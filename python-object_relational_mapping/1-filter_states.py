8ce92ccb14d21cf8ced96072b9c1adb5fc86208d
Alberto <alriffaud@gmail.com>
2024-03-15 18:52:46 -0300

    Modification of 1-filter_states.py file.

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,17 +5,20 @@ from the database hbtn_0e_0_usa:
 """
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
-    db = MySQLdb.connect(
-                            host="localhost",
-                            port=3306,
-                            user=argv[1],
-                            passwd=argv[2],
-                            db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    for row in cur.fetchall():
-        print(row)
-    cur.close()
-    db.close()
+    try:
+        import MySQLdb
+        from sys import argv
+        db = MySQLdb.connect(
+                                host="localhost",
+                                port=3306,
+                                user=argv[1],
+                                passwd=argv[2],
+                                db=argv[3])
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        for row in cur.fetchall():
+            print(row)
+        cur.close()
+        db.close()
+    except Exception as e:
+        print("ERROR: {}".format(e))
