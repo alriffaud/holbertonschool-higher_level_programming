e3c930e466ab2486e92b35338c9bc1548c80aa02
Alberto <alriffaud@gmail.com>
2024-03-15 18:06:20 -0300

    Modification of .\0-select_states.py file.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,12 +9,15 @@ if __name__ == "__main__":
     import sys
     from sys import argv
     try:
+        mysql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        database_name = sys.argv[3]
         db = MySQLdb.connect(
                                 host="localhost",
                                 port=3306,
-                                user=argv[1],
-                                passwd=argv[2],
-                                db=argv[3]
+                                user=mysql_username,
+                                passwd=mysql_password,
+                                db=database_name
                                 )
         cur = db.cursor()
         cur.execute('SELECT * FROM states ORDER BY id ASC')
