549e886be21e3a2a73d7f361d711d93399aea247
Alberto <alriffaud@gmail.com>
2024-03-15 12:37:53 -0300

    Modification of 0-select_states.py files.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,23 +5,17 @@ Write a script that lists all states from the database hbtn_0e_0_usa.
 import MySQLdb
 if __name__ == "__main__":
     import sys
-    try:
-        db = MySQLdb.connect(
-                                host="localhost",
-                                port=3306,
-                                user=sys.argv[1],
-                                password=sys.argv[2],
-                                db=sys.argv[3]
-                                )
-    except MySQLdb.Error as error:
-        sys.exit(1)
+    db = MySQLdb.connect(
+                            host="localhost",
+                            port=3306,
+                            user=sys.argv[1],
+                            password=sys.argv[2],
+                            db=sys.argv[3]
+                            )
     cur = db.cursor()
-    try:
-        cur.execute('SELECT * FROM states ORDER BY id ASC')
-        states = cur.fetchall()
-        for row in states:
-            print(row)
-    except MySQLdb.Error as error:
-        pass
+    cur.execute('SELECT * FROM states ORDER BY id ASC')
+    states = cur.fetchall()
+    for row in states:
+        print(row)
     cur.close()
     db.close()
