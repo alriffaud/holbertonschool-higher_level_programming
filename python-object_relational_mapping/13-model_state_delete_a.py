53d6810bf29b388f68ea37f6ac2bd92e02e7ace2
Alberto <alriffaud@gmail.com>
2024-03-16 15:06:43 -0300

    Modification of 13-model_state_delete_a.py file.

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,8 +14,7 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states_with_a = session.query(State).filter_by(
-            State.name.like('%a%')).first()
+    states_with_a = session.query(State).filter(State.name.like("%a%"))
     for state in states_with_a:
         session.delete(state)
     session.commit()
