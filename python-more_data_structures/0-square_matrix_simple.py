421e60f45518b8fb437c2d2dd61fb70f6c9a7630
alriffaud <100241090+alriffaud@users.noreply.github.com>
2024-03-19 14:36:36 -0300

    Update 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
-def copy_matrix(matrix=[[]]):
-    """
-    This function copies a matrix of integers.
-    """
-    new_matrix = []
-    m = len(matrix)
-    for row in range(m):
-        new_row = []
-        n = len(matrix[row])
-        for j in range(n):
-            new_row.append(matrix[row][j])
-        new_matrix.append(new_row)
-    return (new_matrix)
-
-
 def square_matrix_simple(matrix=[]):
     """
     This function computes the square value of all integers of a matrix.
     """
     if matrix == []:
         return (None)
-    new_matrix = copy_matrix(matrix)
-    m = len(new_matrix)
-    for row in range(m):
-        n = len(new_matrix[row])
-        for j in range(n):
-            new_matrix[row][j] = new_matrix[row][j] ** 2
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for i in row:
+            new_row.append(i ** 2)
+        new_matrix.append(new_row)
     return (new_matrix)
