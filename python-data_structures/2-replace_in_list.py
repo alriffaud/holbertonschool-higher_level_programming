262c700d2e3841b1dcbc04c26db03777cdba403d
Alberto Riffaud <100241090+alriffaud@users.noreply.github.com>
2024-11-09 09:13:03 -0300

    Modification of 2-replace_in_list.py file docuemtation.

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,6 +2,10 @@
 def replace_in_list(my_list, idx, element):
     """
     This function replace an element of a list at a specific position.
+    Args:
+        my_list: list of integers
+        idx: index of the element to replace
+        element: new element to replace
     """
     if idx not in range(len(my_list)):
         return (my_list)
