3fb9b8da54af3c35c24a56a70ab483c8fe4a8597
Alberto Riffaud <100241090+alriffaud@users.noreply.github.com>
2024-11-09 09:16:27 -0300

    Modification of 4-new_in_list.py file documentation.

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,6 +3,10 @@ def new_in_list(my_list, idx, element):
     """
     This function replace an element of a list at a specific position without
     modifying the original list.
+    Args:
+        my_list: list of integers
+        idx: index of the element to replace
+        element: new element to replace
     """
     my_list2 = my_list.copy()
     if idx not in range(len(my_list2)):
