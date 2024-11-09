58b01f3f796892f0664f4098ee4cad00ecc34ef5
Alberto Riffaud <100241090+alriffaud@users.noreply.github.com>
2024-11-09 09:11:27 -0300

    Modification of 1-element_at.py file documetation.

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,6 +2,9 @@
 def element_at(my_list, idx):
     """
     This function retrieves an element from a list.
+    Args:
+        my_list: list of integers
+        idx: index of the element to retrieve
     """
     if idx not in range(len(my_list)):
         return ("None")
