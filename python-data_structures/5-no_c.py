e063668a52715ed5ee1d84b4169ce68c10412e4f
Alberto Riffaud <100241090+alriffaud@users.noreply.github.com>
2024-11-09 09:17:28 -0300

    Modification of 5-no_c.py file documentation.

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,8 @@
 def no_c(my_string):
     """
     This function removes all characters c and C from a string.
+    Args:
+        my_string: string to remove characters c and C
     """
     new_str = ""
     for i in my_string:
