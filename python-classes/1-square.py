f13b0affcf54b003454d44ca5411c6cd5e6a75a2
Alberto Riffaud <100241090+alriffaud@users.noreply.github.com>
2024-11-09 08:41:01 -0300

    Modification of 1-square.py file documentation.

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,8 +7,8 @@ In this module, a Square class is defined.
 class Square:
     """
     This class defines a square.
-    Attributes:
-    - __size: This is the size of the square.
+    Args:
+        size (int): This is the size of the square.
     """
     def __init__(self, size):
         self.__size = size
